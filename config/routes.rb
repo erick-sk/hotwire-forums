b58004f0daeee4ca2cb8075d845b4ef42a80e993
Erick Saavedra <esvdrah@gmail.com>
2022-08-25 15:52:43 -0500

    Add new post on discussion show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :discussions, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :discussions, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :posts, olny: [:create], module: :discussions
+  end
   root 'main#index'
 end
