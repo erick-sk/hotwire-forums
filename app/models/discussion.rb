2f0f055789377355ab610173d0f8070eddcf6467
Erick Saavedra <esvdrah@gmail.com>
2022-08-25 16:47:23 -0500

    Realtime creation of posts for everyone on the discussion page

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -10,9 +10,9 @@ class Discussion < ApplicationRecord
   accepts_nested_attributes_for :posts
 
   # turbo_stream
-  after_create_commit -> { broadcast_prepend_to "discussions"}
-  after_update_commit -> { broadcast_replace_to "discussions"}
-  after_destroy_commit -> { broadcast_remove_to "discussions"}
+  after_create_commit -> { broadcast_prepend_to "discussions" }
+  after_update_commit -> { broadcast_replace_to "discussions" }
+  after_destroy_commit -> { broadcast_remove_to "discussions" }
 
   # friendly url
   def to_param
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,7 @@ class Post < ApplicationRecord
 
   # Action text
   has_rich_text :body
+
+  # turbo_stream
+  after_create_commit -> { broadcast_append_to discussion, partial: "discussions/posts/post", locals: { post: self } }
 end
