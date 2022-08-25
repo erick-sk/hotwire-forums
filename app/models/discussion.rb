6a4542efdd9c44eb21312140401d0b8009a5207f
Erick Saavedra <esvdrah@gmail.com>
2022-08-24 21:04:16 -0500

    Real time discussion functionality

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -4,6 +4,11 @@ class Discussion < ApplicationRecord
   # Validates
   validates :name, presence: true
 
+  # turbo_stream
+  after_create_commit -> { broadcast_prepend_to "discussions"}
+  after_update_commit -> { broadcast_replace_to "discussions"}
+  after_destroy_commit -> { broadcast_remove_to "discussions"}
+
   # friendly url
   def to_param
     "#{id}-#{name.downcase.to_s[0...100]}".parameterize
