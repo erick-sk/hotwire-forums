1cdd2050880738b47004d18eb250840d862c1c16
Erick Saavedra <esvdrah@gmail.com>
2022-08-26 21:41:21 -0500

    Add realtime moving of categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :categories
   devise_for :users
+  resources :categories
   resources :discussions, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :posts, only: [:show, :create, :edit, :update, :destroy], module: :discussions
 
