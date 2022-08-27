de0f48e1acea2de083a97235919f8f5579e85fc8
Erick Saavedra <esvdrah@gmail.com>
2022-08-26 21:18:06 -0500

    Add category filtering

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,4 +12,9 @@ class Category < ApplicationRecord
   after_create_commit -> { broadcast_prepend_to "categories" }
   after_update_commit -> { broadcast_replace_to "categories" }
   after_destroy_commit -> { broadcast_remove_to "categories" }
+
+  # friendly url
+  def to_param
+    "#{id}-#{name.downcase.to_s[0...100]}".parameterize
+  end
 end
